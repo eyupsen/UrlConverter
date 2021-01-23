7b3855bd1789b2d5a8dff8c7fe19d867602c8c19
eyupsen <eyupsen2@gmail.com>
2021-01-23 17:17:43 +0300

    initial commit

---
+++ src/main/java/com/trendyol/urlconverter/UrlconverterApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol.urlconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlconverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlconverterApplication.class, args);
+    }
+
+}
